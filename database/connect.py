d7685a744dcb39a6739e8400f285ce72ef7461f4
pavitra-sudo <pavitrandubey@gmail.com>
2025-12-28 23:07:45 +0530

    connecting sql to python using sqlite3

--- database/connect.py
+++ database/connect.py
@@ -9,7 +9,7 @@ DB_PATH = os.getenv("SQLITE_DB_PATH")
 if not DB_PATH:
     raise RuntimeError("❌ SQLITE_DB_PATH not set in .env")
 
-def get_connection():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
+def get_connection(db_path: str = DB_PATH) -> sqlite3.Connection:
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row  # dict-like rows
     return conn
